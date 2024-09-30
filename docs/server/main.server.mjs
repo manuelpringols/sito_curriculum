b588865bf8681a810404101b32a8202007165de4
manuelpringols <manuelpringols@gmail.com>
2024-09-30 18:57:06 +0200

    rev 1.1

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ZVIBWRE2.mjs";import"./chunk-KX5VN7J6.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-IWQCA2V5.mjs";import"./chunk-KX5VN7J6.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
