7f6cf9bdefe8b181dc64d8267caed7af4b165903
manuelpringols <manuelpringols@gmail.com>
2024-10-14 18:54:14 +0000

    we can do better2

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-CXNTXJNZ.mjs";import"./chunk-EW4UFEY4.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-3HTVXPTU.mjs";import"./chunk-EW4UFEY4.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
