4bff8ede6fe16dc04040981871cf6ae9814bb270
manuelpringols <manuelpringols@gmail.com>
2024-10-23 12:08:34 +0200

    sito curriculum github-backend rev:1.1

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-GOHHIBWZ.mjs";import"./chunk-EW4UFEY4.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-GOIDMJ26.mjs";import"./chunk-EW4UFEY4.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
